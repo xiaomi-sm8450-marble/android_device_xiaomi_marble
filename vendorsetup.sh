b64109469bc43de64ea2bd68f0b46052fab43b69
SharmagRit <sharmagritstuff@gmail.com>
2025-03-30 17:20:29 +0200

    marble: Update vendorsetup.sh for `15-qpr2`

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -11,7 +11,7 @@ echo 'Cloning Basic Call Recorder Tree'
 	git clone https://github.com/xiaomi-sm8450-marble/android_vendor_bcr -b 15 vendor/bcr
 
 echo 'Cloning Device Common Tree'
-	git clone https://github.com/xiaomi-sm8450-marble/android_device_xiaomi_sm8450-common -b 15 device/xiaomi/sm8450-common
+	git clone https://github.com/xiaomi-sm8450-marble/android_device_xiaomi_sm8450-common -b 15-qpr2 device/xiaomi/sm8450-common
 
 echo 'Cloning MemeCamera Tree'
 	git clone https://github.com/xiaomi-sm8450-marble/android_device_xiaomi_miuicamera-marble.git -b 15 device/xiaomi/miuicamera-marble
@@ -25,15 +25,15 @@ echo 'Cloning Hardware Display Tree'
 	git clone https://github.com/xiaomi-sm8450-marble/android_hardware_qcom_display -b 15 hardware/qcom-caf/sm8450/display
 
 echo 'Cloning Hardware Xiaomi Tree'
-	git clone https://github.com/xiaomi-sm8450-marble/android_hardware_xiaomi -b 15 hardware/xiaomi
+	git clone https://github.com/xiaomi-sm8450-marble/android_hardware_xiaomi -b 15-qpr2 hardware/xiaomi
 
 echo 'Cloning Kernel Tree'
-	git clone --depth=1 https://github.com/xiaomi-sm8450-marble/android_kernel_xiaomi_sm8450 -b 15 kernel/xiaomi/sm8450
-	git clone --depth=1 https://github.com/xiaomi-sm8450-marble/android_kernel_xiaomi_sm8450-modules -b 15 kernel/xiaomi/sm8450-modules
-	git clone --depth=1 https://github.com/xiaomi-sm8450-marble/android_kernel_xiaomi_sm8450-devicetrees -b 15 kernel/xiaomi/sm8450-devicetrees
+	git clone --depth=1 https://github.com/xiaomi-sm8450-marble/android_kernel_xiaomi_sm8450 -b 15-qpr2 kernel/xiaomi/sm8450
+	git clone --depth=1 https://github.com/xiaomi-sm8450-marble/android_kernel_xiaomi_sm8450-modules -b 15-qpr2 kernel/xiaomi/sm8450-modules
+	git clone --depth=1 https://github.com/xiaomi-sm8450-marble/android_kernel_xiaomi_sm8450-devicetrees -b 15-qpr2 kernel/xiaomi/sm8450-devicetrees
 
 echo 'Cloning Vendor Tree'
-	git clone https://github.com/xiaomi-sm8450-marble/android_vendor_xiaomi_marble -b 15 vendor/xiaomi/marble
+	git clone https://github.com/xiaomi-sm8450-marble/android_vendor_xiaomi_marble -b 15-qpr2 vendor/xiaomi/marble
 
 echo 'Cloning Vendor Common Tree'
-	git clone https://github.com/xiaomi-sm8450-marble/android_vendor_xiaomi_sm8450-common -b 15 vendor/xiaomi/sm8450-common
+	git clone https://github.com/xiaomi-sm8450-marble/android_vendor_xiaomi_sm8450-common -b 15-qpr2 vendor/xiaomi/sm8450-common
