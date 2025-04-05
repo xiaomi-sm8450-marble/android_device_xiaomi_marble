de3341f2a8d09a3b1af0020fbec256b43efdb9f1
SharmagRit <sharmagritstuff@gmail.com>
2025-04-05 14:36:18 +0200

    marble: Update blobs & fingerprint from marble `V816.0.18.0.UMRMIXM`
    Signed-off-by: SharmagRit <sharmagritstuff@gmail.com>

--- lineage_marble.mk
+++ lineage_marble.mk
@@ -18,8 +18,8 @@ PRODUCT_BRAND := POCO
 PRODUCT_MODEL := 23049PCD8G
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    BuildDesc="marble_global-user 14 UKQ1.230804.001 V816.0.10.0.UMRMIXM release-keys" \
-    BuildFingerprint=POCO/marble_global/marble:14/UKQ1.230804.001/V816.0.10.0.UMRMIXM:user/release-keys \
+    BuildDesc="marble_global-user 14 UKQ1.230804.001 V816.0.18.0.UMRMIXM release-keys" \
+    BuildFingerprint=POCO/marble_global/marble:14/UKQ1.230804.001/V816.0.18.0.UMRMIXM:user/release-keys \
     DeviceName=marble \
     DeviceProduct=marble_global \
     SystemDevice=marble \
