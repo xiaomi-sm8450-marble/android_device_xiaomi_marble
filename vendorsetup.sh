baae53d715cd6eb515f398494d2a1aefaf364fdb
SharmagRit <sharmagritstuff@gmail.com>
2025-08-08 04:01:52 +0000

    marble: Update vendorsetup

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -21,8 +21,15 @@ echo 'Cloning Kernel Tree'
 	git clone --depth=1 https://github.com/xiaomi-sm8450-marble/android_kernel_xiaomi_sm8450-modules.git -b lineage-23.0 kernel/xiaomi/sm8450-modules
 	git clone --depth=1 https://github.com/xiaomi-sm8450-marble/android_kernel_xiaomi_sm8450-devicetrees.git -b lineage-23.0 kernel/xiaomi/sm8450-devicetrees
 
+echo 'Cloning Priv-keys'
+	git clone https://github.com/ProjectInfinity-X/vendor_infinity-priv_keys vendor/infinity-priv/keys
+
 echo 'Cloning Vendor Tree'
 	git clone https://github.com/xiaomi-sm8450-marble/android_vendor_xiaomi_marble.git -b lineage-23.0 vendor/xiaomi/marble
 
 echo 'Cloning Vendor Common Tree'
 	git clone https://github.com/xiaomi-sm8450-marble/android_vendor_xiaomi_sm8450-common.git -b lineage-23.0 vendor/xiaomi/sm8450-common
+
+echo 'Cloning Wfd tree'
+	git clone https://github.com/xiaomi-sm8450-marble/android_device_qcom_wfd device/qcom/wfd
+	git clone https://github.com/xiaomi-sm8450-marble/android_vendor_qcom_wfd.git vendor/qcom/wfd
