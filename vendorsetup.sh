64ea55e0b3acf18e30d08a80c3fd44488f5b1021
SharmagRit <sharmagritstuff@gmail.com>
2025-08-08 04:01:52 +0000

    marble: Update vendorsetup

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -26,3 +26,7 @@ echo 'Cloning Vendor Tree'
 
 echo 'Cloning Vendor Common Tree'
 	git clone https://github.com/xiaomi-sm8450-marble/android_vendor_xiaomi_sm8450-common.git -b lineage-23.0 vendor/xiaomi/sm8450-common
+
+echo 'Cloning Wfd tree'
+	git clone https://github.com/xiaomi-sm8450-marble/android_device_qcom_wfd device/qcom/wfd
+	git clone https://github.com/xiaomi-sm8450-marble/android_vendor_qcom_wfd.git vendor/qcom/wfd
