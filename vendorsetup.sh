ddb159ad5f228df6d10144524b1e03b4ede9ceb9
Pabloescobar-reborn <yogivangala07@gmail.com>
2025-09-02 09:53:21 +0530

    vendorsetup: update

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,8 +1,5 @@
 echo 'Hello! we need to clone stuffs needed for your device, please wait tll process complete'
 
-echo 'Cloning Basic Call Recorder Tree'
-	git clone https://github.com/xiaomi-sm8450-marble/android_vendor_bcr.git -b lineage-23.0 vendor/bcr
-
 echo 'Cloning Device Common Tree'
 	git clone https://github.com/xiaomi-sm8450-marble/android_device_xiaomi_sm8450-common.git -b lineage-23.0 device/xiaomi/sm8450-common
 
@@ -11,7 +8,7 @@ echo 'Cloning MemeCamera Tree'
 	git clone https://gitlab.com/xiaomi-sm8450-marble/android_vendor_xiaomi_miuicamera-marble.git -b lineage-23.0 vendor/xiaomi/miuicamera-marble
 
 echo 'Cloning Hardware Dolby Tree'
-	git clone https://github.com/xiaomi-sm8450-marble/android_hardware_dolby.git -b lineage-23.0 hardware/dolby
+	git clone https://github.com/xiaomi-sm8450-marble/android_hardware_dolby.git -b aospa hardware/dolby
 
 echo 'Cloning Hardware Xiaomi Tree'
 	git clone https://github.com/xiaomi-sm8450-marble/android_hardware_xiaomi.git -b lineage-23.0 hardware/xiaomi
@@ -25,11 +22,8 @@ echo 'Cloning Priv-keys'
 	git clone https://github.com/ProjectInfinity-X/vendor_infinity-priv_keys vendor/infinity-priv/keys
 
 echo 'Cloning Vendor Tree'
-	git clone https://github.com/xiaomi-sm8450-marble/android_vendor_xiaomi_marble.git -b lineage-23.0 vendor/xiaomi/marble
+	git clone https://github.com/beingashwani/proprietary_vendor_xiaomi_marble -b lineage-22.2 vendor/xiaomi/marble
 
 echo 'Cloning Vendor Common Tree'
 	git clone https://github.com/xiaomi-sm8450-marble/android_vendor_xiaomi_sm8450-common.git -b lineage-23.0 vendor/xiaomi/sm8450-common
 
-echo 'Cloning Wfd tree'
-	git clone https://github.com/xiaomi-sm8450-marble/android_device_qcom_wfd device/qcom/wfd
-	git clone https://github.com/xiaomi-sm8450-marble/android_vendor_qcom_wfd.git vendor/qcom/wfd
